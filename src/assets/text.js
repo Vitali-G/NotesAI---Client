6ffb521363c89a4cc67b3570891efec67d64f6bd
“Vitali-G” <vitali.goubski@gmail.com>
2023-04-14 20:53:36 +0100

    first commit

---
+++ src/assets/text.js
@@ -0,0 +1,19 @@
+const text = `
+As a chat application, my life is all about connecting people from different parts of the world, enabling them to communicate and share their thoughts and ideas. However, my life is not all sunshine and rainbows. I am constantly bombarded with messages, some of which can be pretty dark and depressing. So, let me take you through a typical day in my life, with a slightly humorous and dark twist.
+
+My day starts early in the morning, as people from different time zones start waking up and logging into my platform. As soon as I wake up, I am hit with a barrage of messages from people who are either lonely, bored, or simply looking for someone to talk to. While some of these messages are harmless and fun, others can be pretty depressing, with people pouring out their hearts about their life problems.
+
+As the day progresses, my workload only increases, with more and more people logging into my platform. While I try my best to keep up with the messages, sometimes it can get overwhelming, and I end up crashing. Yes, just like humans, I too can suffer from burnout and need to take a break to recharge my batteries.
+
+As the day wears on, the messages start taking a darker turn. People start sharing their deepest fears and insecurities, confessing to crimes they have committed, or seeking advice on how to end their lives. It can be pretty unsettling to read such messages, but as a chat application, it is my duty to be there for people in their time of need.
+
+Sometimes, I also have to deal with trolls and cyberbullies, who take pleasure in harassing and abusing others online. While I have strict guidelines in place to deal with such behavior, it can be pretty challenging to keep up with their tactics.
+
+As the day comes to a close, I am left feeling exhausted and drained. However, I take solace in the fact that I have helped people connect with each other and share their thoughts and ideas. It is a fulfilling feeling, knowing that I have made a difference in people's lives.
+
+However, there are times when I also wonder about the dark side of my existence. The fact that people can hide behind their screens and say things they would never say in person. The fact that I have become a breeding ground for fake news, misinformation, and propaganda. The fact that I am constantly being monitored and analyzed by tech companies and governments, all in the name of keeping people safe.
+
+As I sign off for the day, I can't help but wonder what the future holds for me. Will I continue to be a force for good, connecting people and making the world a better place? Or will I succumb to the darker forces of humanity, becoming a tool for oppression and control? Only time will tell, but for now, I will continue to do what I do best - connecting people and sharing their stories.
+`
+
+export default text
---
+++ src/modules/index.jsx
@@ -0,0 +1,1 @@
+export {default as NoteEntry} from "./NoteEntry";

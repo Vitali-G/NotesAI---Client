c0df3d552e82155d49f905c5524b42eb3178520f
“Vitali-G” <vitali.goubski@gmail.com>
2023-04-17 15:20:18 +0100

    added rich text editor

---
+++ src/modules/RichTextEditor/app.js
@@ -0,0 +1,15 @@
+const elements = document.querySelectorAll('.btn');
+
+elements.forEach(element => {
+    element.addEventListener('click', () => {
+        let command = element.dataset['element']
+
+        if(command == "createLink" || command == "insertImage") {
+            let url = prompt("Enter link: ", 'http://')
+            document.execCommand(command, false, url)
+        } else {
+            document.execCommand(command, false, null);
+        }
+
+    })
+})

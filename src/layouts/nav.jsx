37b777d8327b24e0aa3b825ea52225acd2521b9a
“Vitali-G” <vitali.goubski@gmail.com>
2023-04-17 12:38:01 +0100

    corrected new note page nav

--- src/layouts/nav.jsx
+++ src/layouts/nav.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Navlink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 function Nav() {
   return (
       <>
           <nav>
-              <Navlink to = "/"></Navlink>
-              <Navlink to="/notes">Notes</Navlink>
-              <Navlink to="/user">UserName</Navlink>
-              <Navlink to="/">Logout</Navlink>
-              <Navlink to="/note">CreateNotePage</Navlink>
+              <NavLink to = "/"></NavLink>
+              <NavLink to="/notes">Notes</NavLink>
+              <NavLink to="/user">UserName</NavLink>
+              <NavLink to="/">Logout</NavLink>
+              <NavLink to="/note">New Note</NavLink>
           </nav>
           <Outlet/>
       </>

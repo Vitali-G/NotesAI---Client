a16b465fb57fec28dab9f44d75665669878a148c
“Vitali-G” <vitali.goubski@gmail.com>
2023-04-18 08:46:17 +0100

    progress on the new note page

--- src/modules/index.jsx
+++ src/modules/index.jsx
@@ -1,2 +1,3 @@
 export { default as NoteEntry } from "./NoteEntry";
 export { default as Register } from "./Register"
+export { default as TextEditorBar } from "./TextEditorBar"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-export {  default as createNotePage  } from "./createNotePage";
+export {  default as CreateNotePage  } from "./createNotePage";
 export { default as NotesListPage } from "./NotesListPage";
 export { default as homePage } from './homePage'
 export { default as registerPage } from './registerPage'

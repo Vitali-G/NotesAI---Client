071b4265e72d543fd53a9e6547727daeef262331
Silvia Neves <siltatiana.sn@gmail.com>
2023-04-19 21:42:59 +0100

    fixes github issues

--- src/modules/NoteEntry/index.jsx
+++ src/modules/NoteEntry/index.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useNote } from "../../context/index.jsx";
 import { Link } from "react-router-dom";
-import { Configuration, OpenAIApi } from "openai"
-import TextEditorBar from '../TextEditorBar'
-import "./styles.css"
-import { page } from '../../context';
-
+import { Configuration, OpenAIApi } from "openai";
+import TextEditorBar from "../TextEditorBar";
+import "./styles.css";
+import { page } from "../../context";
 
 const KEY = import.meta.env.VITE_chatGPT_KEY;
 const openai = new OpenAIApi(
@@ -15,7 +13,8 @@ const openai = new OpenAIApi(
 );
 
 export default function NoteEntry() {
-
+  const { setCurrentPage } = page();
+  setCurrentPage(window.location.pathname);
   const [input, setInput] = useState("");
   const [summary, setSummary] = useState("");
   const [loading, setLoading] = useState(true);
@@ -46,7 +45,7 @@ export default function NoteEntry() {
   }
 
   async function getSummary(input) {
-    console.log("ran getsummary");
+    console.log("Ran getSummary");
     const res = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
@@ -61,18 +60,19 @@ export default function NoteEntry() {
     setgotSummary(true);
   }
   async function getQuestions(input) {
+    console.log("Ran getQuestions");
     const res = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
           role: "user",
-          content: `The student has writen a note to help them remember new content from class. To make it easier to remember as much of the content as possible, use it to generate a set of 3 questions which will be used as prompts to remind the student of his notes using spaced repition. Please provide the 3 questions in JSON format. The note is: ${input}`,
+          content: `The student has writen a note to help them remember new content from class. To make it easier to remember as much of the content as possible, use it to generate a set of 3 questions which will be used as prompts to remind the student of his notes using spaced repition. Please provide the 3 questions in JSON format. Ensure that each question has a key of "question" and a corresponding "answer". For example [ { "question": "....?", "answer": "...." } ] The note is: ${input}`,
         },
       ],
     });
-    const data = await res.data.choices[0].message["content"];
+    const data = res.data.choices[0].message["content"];
     setQuestions(data);
-    console.log(questions);
+    console.log(data);
   }
 
   async function getTitle(input) {
@@ -85,9 +85,13 @@ export default function NoteEntry() {
         },
       ],
     });
-    const data = await res.data.choices[0].message["content"];
+    const data = res.data.choices[0].message["content"];
     setTitle(data);
-    console.log(title);
+  }
+
+  function handleTitle(e) {
+    const newInput = e.target.value;
+    setTitle(newInput);
   }
 
   function handleInput(e) {
@@ -95,20 +99,18 @@ export default function NoteEntry() {
     setInput(newInput);
   }
 
-  function handleTitle(e) {
-    const newTitle = e.target.value;
-    setTitle(newTitle);
-  }
-
   function handleSubmit(e) {
-    console.log("handled submit");
     e.preventDefault();
     let noteText = e.target.textContent.replace("Save Note", "");
     setInput(noteText);
+    getQuestions(noteText);
+    !title
+      ? getTitle(noteText)
+      : console.log(
+          "User entered title, AI doesn't need to generate one, day off!"
+        );
     setSummary(" "); // This is so that there is a change to summary and the loading gif plays
     getSummary(noteText);
-    // getQuestions(noteText)
-    // getTitle(noteText)
   }
 
   useEffect(() => {
@@ -129,71 +131,6 @@ export default function NoteEntry() {
       document.execCommand(command, false, url);
     } else {
       document.execCommand(command, false, null);
-
-    const {setCurrentPage} = page()
-    setCurrentPage(window.location.pathname)
-    const [input, setInput] = useState("")
-    const [summary, setSummary] = useState("")
-    const [loading, setLoading] = useState(true)
-    const [questions, setQuestions] = useState([])
-    const [title, setTitle] = useState("")
-    const [gotSummary, setgotSummary] = useState(false)
-
-    async function saveNote(title, input, summary) {
-        const options = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            credentials: "include",
-            body: JSON.stringify({
-                title: title,
-                content: input,
-                summary: summary
-            })
-        }
-        const response = await fetch(`http://localhost:4000/notes/new`, options);
-        console.log(response);
-        const rawData = await response.json()
-        console.log(rawData);
-        setgotSummary(false)
-    }
-
-    async function getSummary(input) {
-        console.log("Ran getSummary")
-        const res = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user",
-                        content: `A user needs a summary of a note he is taking. Please write a 3 line summary of the note below. Make the first 2 lines succinct and informative, and make the third one VERY humorous with the hope that it makes the whole summary a lot more memorable. If you think there the note is not long enough to provide a meaningful summary, please just return a single line summary and end with "The note is not long enough to provide a meaningful summary. The note is: ${input}` }]
-        })
-        const data = res.data.choices[0].message["content"]
-        setSummary(data)
-        setgotSummary(true)
-    }
-    async function getQuestions(input) {
-        console.log("Ran getQuestions")
-        const res = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user",
-                        content: `The student has writen a note to help them remember new content from class. To make it easier to remember as much of the content as possible, use it to generate a set of 3 questions which will be used as prompts to remind the student of his notes using spaced repition. Please provide the 3 questions in JSON format. Ensure that each question has a key of "question" and a corresponding "answer". For example [ { "question": "....?", "answer": "...." } ] The note is: ${input}` }]
-        })
-        const data = res.data.choices[0].message["content"]
-        setQuestions(data)
-        console.log(data);
-    }
-
-    async function getTitle(input) {
-        const res = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user",
-                        content: `A student has written a note, most likely from a new subject they are learning at school. Generate a 5 word title for the note which most accurately describes the general content of the note. The note is: ${input}` }]
-        })
-        const data = res.data.choices[0].message["content"]
-        setTitle(data)
-    }
-
-    function handleTitle(e) {
-        const newInput = e.target.value;
-        setTitle(newInput)
-
     }
   }
 
@@ -212,15 +149,14 @@ export default function NoteEntry() {
       options
     );
 
-
     if (response.ok) {
       console.log("note updated");
       localStorage.clear();
       window.location.assign("/notes");
     } else {
       console.log("note not updated");
+    }
   };
-
   useEffect(() => {
     if (localId) {
       setTitle(localTitle);
@@ -231,7 +167,7 @@ export default function NoteEntry() {
   return (
     <>
       {localId ? <button onClick={updateHandler}>Update Note</button> : ""}
-      <form onSubmit={handleSubmit}>
+      <form>
         <input
           className="input-new-note"
           value={title}
@@ -244,10 +180,10 @@ export default function NoteEntry() {
       <form onSubmit={handleSubmit}>
         <textarea
           value={input}
-          onChange={handleInput}
           className="content"
           id="newNote"
           contentEditable="true"
+          onChange={handleInput}
         ></textarea>
         <button type="submit">Save Note</button>
       </form>
@@ -262,7 +198,6 @@ export default function NoteEntry() {
       ) : (
         ""
       )}
-      {summary ? <p className="summary">{summary}</p> : ""}
       {loading ? (
         <>
           <p>LOADING...</p>
@@ -271,8 +206,7 @@ export default function NoteEntry() {
       ) : (
         ""
       )}
-      {/* {title ? <p className="summary" >{title}</p> : "" } */}
-      {/* {questions ? <p className="summary" >{questions}</p> : "" } */}
+      {/* {summary ? <p className="summary" >{summary}</p> : "" } */}
     </>
   );
 }

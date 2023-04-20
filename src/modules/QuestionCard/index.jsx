60cb2c7693f5c275e0ecace78088ac68d6a6c5d3
“Vitali-G” <vitali.goubski@gmail.com>
2023-04-20 17:45:42 +0100

    conditionally removed delete button from quiz questions on the individual note page

--- src/modules/QuestionCard/index.jsx
+++ src/modules/QuestionCard/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./styles.css";
 
-export default function QuestionCard({ question, answer }) {
+export default function QuestionCard({ question, answer, id, showDelete }) {
+  
+  async function deleteQuestion() {
+    const options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    };
+    const response = await fetch(`http://localhost:4000/questions/${id}`, options);
+    const rawData = await response.json();
+    console.log(rawData);
+    window.location.reload(false);
+  }
+
   return (
     <div className="question-card">
       <h2 className="questions-title-card">{question}</h2>
-      <button className="delete-btn">Delete</button>
+      {showDelete ? <button onClick={deleteQuestion} className="delete-btn">Delete</button> : "" }
       <span className="tooltip">{answer}</span>
     </div>
   );

4155a1b432b8d92f03ba0489fe9c66e42d6cff04
Silvia Neves <siltatiana.sn@gmail.com>
2023-04-20 17:55:43 +0100

    testing

--- src/modules/QuestionCard/index.test.jsx
+++ src/modules/QuestionCard/index.test.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { suite, test, assert, before, after } from "vitest";
 import { screen, render, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import matchers from "@testing-library/jest-dom/matchers";
-expect.extend(matchers);
+import "@testing-library/jest-dom";
 import QuestionCard from "./index.jsx";
 import {
   NoteProvider,
@@ -12,14 +12,18 @@ import {
 } from "../../context/index.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-describe("Question Card", () => {
-  beforeEach(() => {
+suite("Question Card", () => {
+  let question, answer;
+
+  before(() => {
+    question = "What is the capital of France?";
+    answer = "Paris";
     render(
       <PageProvider>
         <UserProvider>
           <NoteProvider>
             <BrowserRouter>
-              <QuestionCard />
+              <QuestionCard question={question} answer={answer} />
             </BrowserRouter>
           </NoteProvider>
         </UserProvider>
@@ -27,7 +31,12 @@ describe("Question Card", () => {
     );
   });
 
-  afterEach(() => {
+  after(() => {
     cleanup();
+  });
+
+  test("renders the question and answer", () => {
+    assert(screen.getByText(question).toBeInTheDocument());
+    assert(screen.getByText(answer).toBeInTheDocument());
   });
 });
